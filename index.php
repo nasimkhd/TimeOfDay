a36901ed7e2655e8c2cdbda2dff101b2de87f369
Nasim khodapanah <51764413+nasimkhd@users.noreply.github.com>
2021-03-03 02:48:42 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+
+function getMessage($timeOfDay){
+
+    if ($timeOfDay == 0) { // 0 = Morning
+        $message = 'Good Morning';
+    }else if($timeOfDay == 1){
+        $message = 'Good Afternoon';
+    } else {
+        $message = 'Good Evening';
+    }
+    
+
+    //$message = 'Hello, this is server..';
+    echo $message;
+}
+
+getMessage(3);
